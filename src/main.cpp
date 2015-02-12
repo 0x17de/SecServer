ff5f8afaae1f5103251f803d232fadcfef4bc058
Manuel Herrmann <it@icetruck.de>
2015-02-12 20:20:59 +0100

    name restrictions

--- src/main.cpp
+++ src/main.cpp
@@ -118,8 +118,14 @@ int main() {
                 || user == "") {
                     authSuccess = false;
                 } else {
+                    static const string allowedCharacters("/.,<>?\\[]!#$%^*()_+=-");
                     for (char c : user) {
-                        if (c == ':') {
+                        if ((c >= 'a' && c <= 'z')
+                        || (c >= 'A' && c <= 'Z')
+                        || (c >= '0' && c <= '9')
+                        || allowedCharacters.find(c)) {
+                            // Ok.
+                        } else {
                             authSuccess = false;
                             break;
                         }
